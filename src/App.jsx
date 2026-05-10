6f04ebf52765e90d8df53cbf32d0b9b84e92f96b
Aditya-mac <dr.adityakumar2001@gmail.com>
2026-05-10 16:49:30 +0530

    removed unused files + navbar click working

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,16 @@
-import React from 'react';
-import { Routes, Route, useLocation } from 'react-router-dom';
-import { AnimatePresence } from 'framer-motion';
 import { MainLayout } from './layouts/MainLayout';
 import { Home } from './pages/Home';
-import { Projects } from './pages/Projects';
 import { Preloader } from './components/ui/Preloader';
 import { CustomCursor } from './components/ui/CustomCursor';
 import './App.css';
 
 function App() {
-  const location = useLocation();
-
   return (
     <>
       <Preloader />
       <CustomCursor />
       <MainLayout>
-        <AnimatePresence mode="wait">
-          <Routes location={location} key={location.pathname}>
-            <Route path="/" element={<Home />} />
-            <Route path="/projects" element={<Projects />} />
-          </Routes>
-        </AnimatePresence>
+        <Home />
       </MainLayout>
     </>
   );
